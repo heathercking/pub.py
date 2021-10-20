f6a1a027f8d40d9007c3ad97dff393fe85075948
olly <olly.marsters@yahoo.co.uk>
2021-10-20 11:40:29 +0100

    set up initial tests

--- run_tests.py
+++ run_tests.py
@@ -1,5 +1,8 @@
 import unittest
 from tests.pub_test import TestPub
+# from tests.customer_test import Customer
+# from tests.drinks_test import Drinks
+# from tests.till_test import Till
 
 if __name__ == '__main__':
     unittest.main()
