27d5add08219f4c40350b67daab416885a9fe8b5
olly <olly.marsters@yahoo.co.uk>
2021-10-20 15:53:42 +0100

    completed MVP

--- tests/pub_test.py
+++ tests/pub_test.py
@@ -2,11 +2,15 @@ import unittest
 import pdb
 from src.pub import Pub
 from src.drinks import Drinks
+from src.till import Till
+from src.customer import Customer
 
 class TestPub(unittest.TestCase):
     
     def setUp(self):
         self.ourpub = Pub("The Prancing Pony")
+        self.ourtill = Till(1000)
+        self.ourcustomer = Customer("Barry Brexit", 60)
 
     def test_pub__has_name(self):
         self.assertEqual("The Prancing Pony", self.ourpub.name)
@@ -30,12 +34,15 @@ class TestPub(unittest.TestCase):
         self.assertEqual(1, len(self.ourpub.drinks))
 
 
-
-
-
-
-    # def test_customer_can_buy_drink(self):
-    #     self.assertEqual(1009.1, self.till.total_cash)
-    #     self.assertEqual(50.9, self.customer.wallet)
+    def test_customer_can_buy_drink(self):
+        new_drink1 = Drinks("Brewdog", 9.10)
+        self.ourpub.add_drink(new_drink1)
+        new_drink2 = Drinks("MD2020", 3.0)
+        self.ourpub.add_drink(new_drink2)
+        drink_price = self.ourpub.find_drink_price(new_drink1.name)
+        self.ourcustomer.reduce_cash_from_wallet(drink_price)
+        self.ourtill.increase_cash_in_till(drink_price)
+        self.assertEqual(1009.1, self.ourtill.total_cash)
+        self.assertEqual(50.9, self.ourcustomer.wallet)
     
 
